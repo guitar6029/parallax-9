12062467538c59eb6e0ca917f512a9f19c77501a
Josh <jsbusiness305@gmail.com>
2025-11-07 11:12:23 -0500

    updated ui

--- app/components/indexView/NetworkExpands.vue
+++ app/components/indexView/NetworkExpands.vue
@@ -1,29 +1,30 @@
 <template>
     <section class="sect-container mt-20 relative">
-        <!-- <div
-            class="hidden lg:absolute lg:-top-150  lg:inset-0 lg:h-200 lg:w-full lg:flex flex-col items-center justify-center gap-2 bg-(--pz-neon)/10 shape-4 border-2 border-r-2 border-(--pz-neon)">
-            <h1 class="absolute section-title z-20">The Network Expands</h1>
-        </div> -->
 
-        <div class="relative hidden  lg:flex lg:flex-col lg:items-center lg:justify-center lg:h-200 lg:w-full">
+        <div class="relative hidden  lg:flex lg:flex-col lg:items-center lg:justify-center lg:h-100 lg:w-full">
             <!-- Background Shape Layer -->
-            <div class="absolute top-0 inset-0 bg-(--pz-neon)/10 shape-4 z-10">
+            <div class="absolute top-0 inset-0 bg-(--pz-neon)/10 bevel z-10">
             </div>
-            <div class="absolute top-20 inset-0 bg-(--pz-neon)/20 shape-4 z-11">
+            
+            <div class="absolute top-10 inset-0 bg-(--pz-neon)/10 bevel z-11">
             </div>
-            <div class="absolute top-40 inset-0 bg-(--pz-neon)/30 shape-4 z-12">
+
+            <div class="absolute top-20 inset-0 bg-(--pz-neon)/20 bevel z-12">
             </div>
-            <div class="absolute top-60 inset-0 bg-(--pz-neon)/40 shape-4 z-13">
+
+            <div class="absolute top-30 inset-0 bg-(--pz-neon)/30 bevel z-13">
+            </div>
+
+            <div class="absolute top-40 inset-0 bg-(--pz-neon)/40 bevel z-14">
+            </div>
+            <div class="absolute top-50 inset-0 bg-(--pz-neon)/50 bevel z-15">
             </div>
 
             <!-- Foreground Text Layer -->
-            <h1 class="absolute section-title z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <h1 class="absolute section-title z-100 bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2">
                 The Network Expands
             </h1>
         </div>
-
-
-
         <h1 class="section-title block lg:hidden">The Network Expands</h1>
         <span class="text-3xl">Over 1,500 Curated Memory Constructs Activated</span>
         <p class="text-3xl">Each CMC is a handcrafted neural immersion —
