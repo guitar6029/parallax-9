8827963527ba2cfef50bc2c9bf13b2ef06d23608
Josh <jsbusiness305@gmail.com>
2025-11-09 10:25:30 -0500

    updated logic and ui

--- app/pages/catalog.vue
+++ app/pages/catalog.vue
@@ -1,60 +1,14 @@
 <script setup lang="ts">
 import { onMounted, onBeforeUnmount, ref, computed } from 'vue';
-import CMSItems from '@/assets/data/Parallax9Data.json'
+import CMC_EXPERIENCES from '@/assets/data/Parallax9Data.json'
 import type { CMC } from '~/types/CMC';
+import { throttle } from '#imports';
 
+const STEP_X = 300;
+const STEP_Y = 20;
+const STEP_Z = 60;
 
-const CMSExperiences = ref<CMC[]>(CMSItems)
-const activeCMS = ref<CMC | null>(null)
-const activeIndex = ref(0)
-const maxIndex = ref(CMSItems.length - 1)
-
-
-
-function handleKey(e: KeyboardEvent) {
-    console.log("current active index : ", activeIndex.value)
-    if (e.key === 'ArrowLeft') {
-        console.log('Left arrow pressed');
-        // user wants to go to previous one 
-        if (activeIndex.value === 0) {
-            //do nothing
-            console.log("no more, at the first one")
-            activeCMS.value = CMSExperiences.value[0] as CMC
-            return
-        } else {
-            console.log('going back by 1')
-            activeIndex.value = activeIndex.value - 1
-            // set the active CMC
-            activeCMS.value = CMSExperiences.value[activeIndex.value - 1] as CMC
-
-        }
-    } else if (e.key === 'ArrowRight') {
-        if (activeIndex.value === maxIndex.value) {
-            //do nothing
-            console.log("no more, next does not exist")
-            return
-        } else {
-            console.log("going forward by 1")
-            activeIndex.value = activeIndex.value + 1
-            // set the CMC
-            activeCMS.value = CMSExperiences.value[activeIndex.value] as CMC
-        }
-    } else if (e.key === ' ' || e.key === 'Space') {
-        console.log('Spacebar pressed');
-    }
-}
-
-onMounted(() => {
-    console.log("CMC items : ", CMSItems);
-    window.addEventListener("keydown", handleKey)
-})
-
-onBeforeUnmount(() => {
-    window.removeEventListener("keydown", handleKey)
-})
-
-
-// definePageMeta({ layout: 'main' })
+//definePageMeta({ layout: 'main' })
 useHead({
     title: 'Catalog — Parallax-9',
     meta: [
@@ -67,8 +21,44 @@ useHead({
 })
 
 
-const cmc = computed(() => {
-    return activeCMS.value
+function handleKey(e: KeyboardEvent) {
+    if (e.key === 'ArrowLeft') {
+        // user wants to go to previous one 
+        if (activeIndex.value !== 0) {
+            return activeIndex.value = activeIndex.value - 1
+        }
+    } else if (e.key === 'ArrowRight') {
+        if (activeIndex.value < maxIndex.value) {
+            return activeIndex.value = activeIndex.value + 1
+        }
+    } else if (e.key === ' ' || e.key === 'Space') {
+        console.log('Spacebar pressed');
+        e.preventDefault()
+    }
+}
+
+
+const handleKeyThrottled = throttle(handleKey, 150)
+const CMCExperiences = ref<CMC[]>(CMC_EXPERIENCES)
+const activeIndex = ref(0)
+
+onMounted(() => {
+    window.addEventListener("keydown", handleKeyThrottled)
+})
+
+onBeforeUnmount(() => {
+    window.removeEventListener("keydown", handleKeyThrottled)
+    handleKeyThrottled.cancel()
+})
+
+
+// returns the object of the active index from CMC_EXPERIENCES
+const activeCMC = computed(() => {
+    return CMCExperiences.value[activeIndex.value]
+})
+
+const maxIndex = computed(() => {
+    return CMCExperiences.value.length - 1
 })
 
 </script>
@@ -78,26 +68,27 @@ const cmc = computed(() => {
 
 
         <!-- THE CMC MONITOR -->
-        <div class="absolute shadow-neutral-700 shadow-2xl bottom-150 left-120 w-200 h-200 bg-(--pz-bg-2) -skew-x-12 -skew-y-12 flex flex-col items-center justify-center">
-                <!-- <span class="font-tech text-4xl tracking-[.5em]">Please LOAD CMC</span> -->
-                 <div class="flex flex-col gap-2 items-center justify-center">
-                    <h1>{{ cmc?.title }}</h1>
-                    <span>{{ cmc?.intensity }}</span>
-                 </div>
+        <div
+            class="absolute bottom-180 left-180 w-200 h-200 bg-(--pz-bg-2) -skew-x-1 -skew-y-12 flex flex-col items-center justify-center">
+            <!-- <span class="font-tech text-4xl tracking-[.5em]">Please LOAD CMC</span> -->
+            <div class="flex flex-col gap-2 items-center justify-center">
+                <h1>{{ activeCMC?.title }}</h1>
+                <span>Intensity {{ activeCMC?.intensity }}</span>
+            </div>
         </div>
 
-    
-        <div class="absolute bottom-50 left-120 [perspective:100rem]">
 
-            <div
-                class="relative gap-2 w-[720px] h-[260px] [transform-style: preserve-3d] [transform-origin: 0%_100%] [transform:rotateX(55deg)_rotateZ(-20deg)]">
+        <div class="absolute bottom-50 left-120 perspective-[100rem]">
 
-                <div v-for="(item, index) in CMSExperiences" :key="item.id">
-                    <div :class="['absolute transition-transform duration-400 ease-out top-0 left-0 flex items-center justify-center  w-64 h-40 rounded-2xl border-2', { 'border-(--pz-neon)': activeIndex === item.id - 1 }]"
+            <div tabindex="0"
+                class="relative w-[720px] h-[260px] [transform-style: preserve-3d] [transform-origin: 0%_100%] transform-[rotateX(55deg)_rotateZ(-20deg)]">
+
+                <div v-for="(item, index) in CMCExperiences" :key="item.id">
+                    <div :class="['absolute transition-transform duration-300 ease-out top-0 left-0 flex items-center justify-center  w-64 h-40 rounded-2xl border-2 will-change-transform backface-hidden', { 'border-(--pz-neon)': activeIndex === index }]"
                         :style="`transform:
-            translateX(${((index - activeIndex) * 300)}px)
-            translateY(${-(index - 1) * 20}px)
-            translateZ(${(index - 1) * 60}px)
+            translateX(${((index - activeIndex) * STEP_X)}px)
+            translateY(${-(index - activeIndex) * STEP_Y}px)
+            translateZ(${(index - activeIndex) * STEP_Z}px)
             rotateX(32deg) rotateY(6deg)`">
 
                         <div class="absolute top-1/2 -translate-y-1/2">
@@ -106,7 +97,7 @@ const cmc = computed(() => {
 
                         <!-- index + 1 since 0 based -->
                         <div class="relative -bottom-40  -skew-y-2 -skew-x-2 flex flex-col items-center gap-2"
-                            v-if="activeIndex === (item.id - 1)">
+                            v-if="activeIndex === index">
                             <!-- <span>this one</span> -->
                             <Icon name="material-symbols:arrow-outward" size="3rem" class="-rotate-40" />
                             <span class="text-4xl">Space (Load CMC)</span>
@@ -117,19 +108,6 @@ const cmc = computed(() => {
             </div>
 
         </div>
-
-        <!-- MAYBE FOR LATER -->
-        <!-- <div class="absolute -bottom-10 -left-10 [perspective:800px]">
-
-            <div
-                class="relative gap-2 w-[720px] h-[260px] [transform-style: preserve-3d] [transform-origin: 0%_100%] [transform:rotateX(55deg)_rotateZ(-20deg)]">
-                <div class="text-4xl font-tech">
-                    this 
-                </div>
-                
-            </div>
-        </div> -->
-
     </section>
 
 </template>
