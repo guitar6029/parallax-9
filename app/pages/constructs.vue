458bc868668d3e3b3c449b3736076a12541a1fc6
Josh <jsbusiness305@gmail.com>
2025-11-09 13:38:38 -0500

    updated logic

--- app/pages/constructs.vue
+++ app/pages/constructs.vue
@@ -8,6 +8,9 @@ const STEP_X = 450;
 const STEP_Y = 10;
 const STEP_Z = 60;
 
+
+const currentWindowSize = ref(0)
+
 //definePageMeta({ layout: 'main' })
 useHead({
     title: 'Catalog — Parallax-9',
@@ -20,6 +23,12 @@ useHead({
     ],
 })
 
+
+function updateWindowSize() {
+    if (typeof window !== 'undefined') {
+        currentWindowSize.value = window.innerWidth
+    }
+}
 
 function handleKey(e: KeyboardEvent) {
     if (e.key === 'ArrowLeft') {
@@ -39,16 +48,26 @@ function handleKey(e: KeyboardEvent) {
 
 
 const handleKeyThrottled = throttle(handleKey, 150)
+const handleResizeThrottled = throttle(updateWindowSize, 150)
 const CMCExperiences = ref<CMC[]>(CMC_EXPERIENCES)
 const activeIndex = ref(0)
 
 onMounted(() => {
+    //get window size
+    updateWindowSize()
+
     window.addEventListener("keydown", handleKeyThrottled)
+    //window resize 
+    window.addEventListener("resize", handleResizeThrottled)
 })
 
 onBeforeUnmount(() => {
     window.removeEventListener("keydown", handleKeyThrottled)
     handleKeyThrottled.cancel()
+
+    window.removeEventListener("resize", handleResizeThrottled)
+    handleResizeThrottled.cancel()
+
 })
 
 
@@ -61,54 +80,45 @@ const maxIndex = computed(() => {
     return CMCExperiences.value.length - 1
 })
 
+
 </script>
 
 <template>
     <section class="sect-container h-screen relative">
-
-
-        <!-- stack 2 -->
-         <div class="absolute bottom-180 left-120 w-650 h-250 bg-(--pz-bg-2)/30 -skew-x-1 -skew-y-12">
-         </div>
-        <!-- THE CMC MONITOR 1 stack -->
-        <div
-            class="scale-50 absolute bottom-200 left-180 w-500 h-200 bg-(--pz-bg-2) -skew-x-1 -skew-y-12 flex flex-col items-center justify-center">
-            <div class="flex flex-col gap-2 items-center justify-center">
-                <h1>{{ activeCMC?.title }}</h1>
-                <span>Intensity {{ activeCMC?.intensity }}</span>
+        <div class="absolute bottom-50 left-90 perspective-[150rem] pointer-events-none">
+            <div
+                class="absolute bottom-75 left-1/2 translate-x-[-50%] w-420 h-200 bg-(--pz-bg-2) -skew-x-1 -skew-y-12 flex flex-col items-center justify-center">
+                <div class="flex flex-col gap-2 items-center justify-center">
+                    <h1 class="cyber text-5xl">{{ activeCMC?.title }}</h1>
+                    <span>Intensity {{ activeCMC?.intensity }}</span>
+                </div>
             </div>
-        </div>
 
 
-        <!-- BELT -->
-         <!-- <div class="absolute bottom-50 left-120 perspective-[100rem] border-2" >
-            <div class="relative w-[1720px] h-[260px] [transform-style: preserve-3d] [transform-origin: 0%_100%] transform-[rotateX(55deg)_rotateZ(-20deg)]">
 
-            </div>
-         </div> -->
-
-
-        <div class="absolute bottom-50 left-120 perspective-[150rem] scale-50">
 
             <div tabindex="0"
                 class="relative w-[720px] h-[260px] [transform-style: preserve-3d] [transform-origin: 0%_100%] transform-[rotateX(55deg)_rotateZ(-20deg)]">
 
                 <div v-for="(item, index) in CMCExperiences" :key="item.id">
-                    <div :class="['absolute transition-transform duration-300 ease-out top-0 left-0 flex items-center justify-center  w-100 h-150 rounded-2xl border-2 will-change-transform backface-hidden', { 'border-(--pz-neon)': activeIndex === index }]"
+                    <div :class="['cmc-card absolute transition-transform duration-300 ease-out top-0 left-0 flex items-center justify-center border-4 w-100 h-150 rounded-2xl  will-change-transform backface-hidden', { 'border-(--pz-yellow)': activeIndex === index }]"
                         :style="`transform:
-            translateX(${((index - activeIndex) * STEP_X )}px)
+            translateX(${((index - activeIndex) * STEP_X)}px)
             translateY(${-(index - activeIndex) * STEP_Y}px)
             translateZ(${(index - activeIndex) * STEP_Z}px)
             rotateX(32deg) rotateY(6deg)`">
+
+                        <div class="absolute w-5 h-5 bg-(--pz-yellow) top-2 left-2">
+
+                        </div>
 
                         <div class="absolute top-1/2 -translate-y-1/2">
                             <span class="text-4xl">{{ item.title }}</span>
                         </div>
 
                         <!-- index + 1 since 0 based -->
-                         <!-- -skey-y-2 -skew-x-2 -->
-                        <div class="relative -bottom-40 flex flex-col items-center gap-2"
-                            v-if="activeIndex === index">
+                        <!-- -skey-y-2 -skew-x-2 -->
+                        <div class="relative -bottom-40 flex flex-col items-center gap-2" v-if="activeIndex === index">
                             <!-- <span>this one</span> -->
                             <!-- <Icon name="material-symbols:arrow-outward" size="3rem" class="-rotate-40" /> -->
                             <span class="text-4xl">Load CMC</span>
@@ -122,3 +132,5 @@ const maxIndex = computed(() => {
     </section>
 
 </template>
+
+<style scoped></style>
