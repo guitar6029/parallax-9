ecb9cc276199b543e5d3b8b24d0bf6682f5d384c
jsdev305 <jsdev305@gmail.com>
2025-11-13 18:50:32 -0500

    moved all of the three js logic into a new component - The Core Cognition
    added scenes to the page, added animation for the text animation

--- app/pages/the-fragment-protocol.vue
+++ app/pages/the-fragment-protocol.vue
@@ -1,118 +1,76 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, ref } from "vue";
-import {throttle} from '@/utils/throttle'
-import * as THREE from "three";
-const divRef = ref<HTMLElement | null>(null);
+import TheCoreCognition from "~/components/TheCoreCognition.vue";
+import { onMounted, ref, computed } from "vue";
+const SCENE_SWITCHER_TIME_IN_SECONDS = 10500;
+const initialView = ref<"intro" | "theCoreCognition">("intro");
 
-
-function handleResize(){
-  if (!divRef.value || !renderer || !camera) return
-  const width = divRef.value.clientWidth || window.innerWidth
-  const height = divRef.value.clientHeight || window.innerHeight
-
-  renderer.setSize(width, height)
-  camera.aspect = width / height
-  camera.updateProjectionMatrix()
-}
-
-
-const handleResiseThrottled = throttle(handleResize, 150)
-let renderer: THREE.WebGLRenderer | null = null;
-let camera: THREE.PerspectiveCamera | null = null;
-let cube: THREE.Mesh | null = null;
-let scene: THREE.Scene | null = null;
-let frameId: number | null = null;
-
-const clock = new THREE.Clock() 
-
-function animate() {
-  if (!renderer || !scene || !camera || !cube) return;
-
-  const t = clock.getElapsedTime()
-
-  // rotate
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  //breathe params
-  const amplitude = 0.12 // grows/ shrinks
-  const speed = 0.5 // how fast it breathes
-  const scale = 1 + amplitude * Math.sin(t * speed)
-  cube.scale.set(scale, scale, scale)
-
-  //render
-  renderer.render(scene, camera);
-
-  // schedule frame
-  frameId = requestAnimationFrame(animate);
-}
+const scenes = {
+  cryptic: [
+    "COGNITION CORE: ONLINE",
+    "SIGNAL ACCEPTED",
+    "INITIALIZING FRAGMENT PROTOCOL…",
+  ],
+  sentient: [
+    "THE CORE IS AWAKE",
+    "IDENTITY SIGNAL: RECEIVED",
+    "PREPARING FRAGMENTS…",
+  ],
+  deepSciFi: [
+    "SOMETHING IS WATCHING",
+    "YOUR MIND HAS BEEN RECOGNIZED",
+    "UNSEALING THE FRAGMENTS…",
+  ],
+};
 
 onMounted(() => {
-  if (!divRef.value) return;
-
-  //add resize event
-  window.addEventListener("resize", handleResiseThrottled)
-
-  // init width and height
-  const width = divRef.value.clientWidth || window.innerWidth;
-  const height = divRef.value.clientHeight || window.innerHeight;
-
-  //initialize the scene
-  scene = new THREE.Scene();
-
-  //lights
-  const ambient = new THREE.AmbientLight(0xffffff, 0.4)
-  scene.add(ambient)
-
-  const dir = new THREE.DirectionalLight(0xffffff, 1)
-  dir.position.set(4,6,2)
-  scene.add(dir)
-
-  //camera init
-  camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-  camera.position.z = 5;
-
-  //renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(width, height);
-  renderer.setClearColor(0x050510);
-  divRef.value?.appendChild(renderer.domElement);
-
-  // texture 
-  const textureLoader = new THREE.TextureLoader()
-  //load the image
-  const colorMap = textureLoader.load('/textures/metal_plate_02_diff_4k.jpg')
-
-  //create a shape
-  //const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const geometry = new THREE.IcosahedronGeometry(1, 0);
-  //const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
-  const material = new THREE.MeshStandardMaterial({
-    map: colorMap,
-    metalness: 0.8,
-    roughness: 0.4
-  })
-  cube = new THREE.Mesh(geometry, material);
-
-  scene.add(cube);
-
-  //animate
-  animate();
+  setTimeout(() => {
+    initialView.value = "theCoreCognition";
+  }, SCENE_SWITCHER_TIME_IN_SECONDS);
 });
 
-onBeforeUnmount(() => {
-  //clean up for later
-  if (frameId !== null) {
-    cancelAnimationFrame(frameId);
-  }
-
-  //remove and cancel the resize event
-  window.removeEventListener('resize', handleResiseThrottled)
-  handleResiseThrottled.cancel()
+// COMPUTED
+const currentView = computed(() => {
+  return initialView.value;
 });
 </script>
 
 <template>
   <section class="sect-container h-screen">
-    <div ref="divRef"></div>
+    <div
+      v-if="currentView === 'intro'"
+      class="flex flex-col items-center justify-center gap-10 text-4xl font-tech uppercase"
+    >
+      <span
+        v-for="(line, index) in scenes['sentient']"
+        :key="index"
+        class="fade-in"
+      >
+        {{ line }}
+      </span>
+    </div>
+    <TheCoreCognition v-if="currentView === 'theCoreCognition'" />
   </section>
 </template>
+
+<style scoped>
+.fade-in {
+  opacity: 0;
+  animation: fadeIn 10s forwards ease-out;
+}
+
+.fade-in:nth-child(1) {
+  animation-delay: 1s;
+}
+.fade-in:nth-child(2) {
+  animation-delay: 3.25s;
+}
+.fade-in:nth-child(3) {
+  animation-delay: 6.5s;
+}
+
+@keyframes fadeIn {
+  to {
+    opacity: 1;
+  }
+}
+</style>
