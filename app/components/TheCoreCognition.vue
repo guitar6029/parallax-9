2e57f89f38af252db359f4307a70db3bbfdfd323
jsdev305 <jsdev305@gmail.com>
2025-11-16 22:43:33 -0500

    updated ui for the CoreCognition

--- app/components/TheCoreCognition.vue
+++ app/components/TheCoreCognition.vue
@@ -1,8 +1,30 @@
 <script setup lang="ts">
+import TheCore from "@/assets/img/TheCore.png";
 </script>
 
 <template>
-  <div class="sect-section h-screen">
+  <div class="sect-container h-screen">
     <h1 class="cyber text-4xl">The Core Cognition</h1>
+    <div class="max-w-200 breathe">
+      <img :src="TheCore" alt="the core entity" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.breathe {
+  animation: breatheA 10s ease-in infinite;
+}
+
+@keyframes breatheA {
+  0% {
+    scale: 1;
+  }
+  50% {
+    scale: 1.3;
+  }
+  100% {
+    scale: 1;
+  }
+}
+</style>
