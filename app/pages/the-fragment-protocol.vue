84e74cff1a47a387e253e08a59bf9a110de4f728
jsdev305 <jsdev305@gmail.com>
2025-11-26 18:28:57 -0500

    added the default layout

--- app/pages/the-fragment-protocol.vue
+++ app/pages/the-fragment-protocol.vue
@@ -4,7 +4,7 @@ import { onMounted, ref, computed } from "vue";
 const SCENE_SWITCHER_TIME_IN_SECONDS = 10500;
 const initialView = ref<"intro" | "theCoreCognition">("intro");
 
-// definePageMeta({ layout: 'main' })
+definePageMeta({ layout: 'main' })
 
 const scenes = {
   cryptic: [
